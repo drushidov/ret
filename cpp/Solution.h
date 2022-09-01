0dba80b35d7a99fd4c6723878a3bcd828454d094
Daniel Rushidov <daniel.rushidov@gmail.com>
2022-09-01 20:34:46 +0300

    Add base eye pattern pixels before checking others

--- cpp/Solution.h
+++ cpp/Solution.h
@@ -25,19 +25,21 @@ public:
   }
 
   void computeImage(PackedImage& image) {
-      int32_t width = image.resolution.width;
-      int32_t height = image.resolution.height;
+      int32_t imageWidth = image.resolution.width;
+      int32_t imageHeight = image.resolution.height;
       int pixelsCount = image.pixels.size();
 
-      for (int i = 0; i < pixelsCount; i++) {
-          Pixel currentPixel = image.pixels.at(i);
+      for (int pixelIndex = 0; pixelIndex < pixelsCount; pixelIndex++) {
+          Pixel currentPixel = image.pixels.at(pixelIndex);
 
           if (currentPixel.red >= 200) {
               std::list<int> matchedPixelsIndexes;
 
+              addBasePatternPixels(image.pixels, pixelIndex, matchedPixelsIndexes, imageWidth);
+
               for each (EyePattern pattern in EYE_PATTERNS)
               {
-                  checkPattern(image.pixels, i, matchedPixelsIndexes, width, height, pattern);
+                  checkPattern(image.pixels, pixelIndex, matchedPixelsIndexes, imageWidth, imageHeight, pattern);
               }
 
               for each (int matchedPixelIndex in matchedPixelsIndexes)
@@ -50,20 +52,27 @@ public:
       }
   }
 
-  void checkPattern(std::vector<Pixel>& pixels, size_t startingPixelIndex, std::list<int>& matchedPixelsIndexes, int32_t width, int32_t height, EyePattern pattern) {
-      if (width - (startingPixelIndex % width) < 5) {
-          // Not enough horizontal space for pattern.
-          return;
+  void addBasePatternPixels(std::vector<Pixel>& pixels, size_t startingPixelIndex, std::list<int>& matchedPixelsIndexes, int32_t width) {
+      for (size_t row = 0; row < 5; row++)
+      {
+          for (size_t col = 0; col < 5; (row == 0 || row == 4) ? col++ : col += 4)
+          {
+              int currentPixelIndex = getPixelIndexByOffset(startingPixelIndex, row, col, width);
+              Pixel currentPixel = pixels.at(currentPixelIndex);
+              matchedPixelsIndexes.push_back(currentPixelIndex);
+          }
       }
+  }
 
+  void checkPattern(std::vector<Pixel>& pixels, size_t startingPixelIndex, std::list<int>& matchedPixelsIndexes, int32_t width, int32_t height, EyePattern pattern) {
       int currentPatternMatchesCount = 0;
 
-      for (size_t i = 0; i < 5; i++)
+      for (size_t row = 1; row < 4; row++)
       {
-          for (size_t j = 0; j < 5; j++)
+          for (size_t col = 1; col < 4; col++)
           {
-              const char currentPatternChar = pattern.at(i)[j];
-              int currentPixelIndex = (startingPixelIndex + j) + (i * width);
+              const char currentPatternChar = pattern.at(row)[col];
+              int currentPixelIndex = getPixelIndexByOffset(startingPixelIndex, row, col, width);
               Pixel currentPixel = pixels.at(currentPixelIndex);
 
               if (currentPatternChar == ' ') {
@@ -75,7 +84,7 @@ public:
                   currentPatternMatchesCount++;
               }
               else {
-                  // Remove pixels that match parts of current pattern.
+                  // Remove pixels that match parts of current pattern
                   while (currentPatternMatchesCount--) {
                       matchedPixelsIndexes.pop_back();
                   }
@@ -84,6 +93,10 @@ public:
               }
           }
       }
+  }
+
+  int32_t getPixelIndexByOffset(int32_t startingIndex, int32_t rowOffset, int32_t columnOffset, int32_t imageWidth) {
+      return (startingIndex + columnOffset) + (rowOffset * imageWidth);
   }
 };
 
